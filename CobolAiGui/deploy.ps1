e6b8dbb69b55cf8fc743972d1152706527582960
Peter Fisk <peter.fisk@gmail.com>
2026-04-04 09:58:57 -0400

    init

--- CobolAiGui/deploy.ps1
+++
@@ -1,2 +0,0 @@
-cd bin/debug/net10.0/browser-wasm/publish/wwwroot
-npx wrangler pages --branch=main --project-name=cobol-ai-studio-site deploy .
--- CobolAiGui/g.ps1
+++
@@ -1,4 +0,0 @@
-pushd
-cd bin/debug/net10.0/browser-wasm/publish/wwwroot
-python -m http.server
-popd
